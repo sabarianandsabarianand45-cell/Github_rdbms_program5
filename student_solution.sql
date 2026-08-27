3ed6f5f3e201aaa235618acd2c39b882d8b3d87d
sabarianandsabarianand45-cell <sabarianandsabarianand45@gmail.com>
2026-08-27 10:34:45 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,7 @@
-
+USE Student;
+INSERT INTO Student(StudentID,StudentName,Gender,Department)
+VALUES
+(1001,'Arun','Male',101),
+(1002,'Divya','Female',102),
+(1003,'Karthik','Male',101);
+SELECT*FROM Student;
